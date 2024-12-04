50e96211005529ede0471f5595f7dac1a0863cc5
Rajkumar <44047368+Rajkumar0208@users.noreply.github.com>
2024-12-03 21:14:36 -0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,22 @@
+//First Java Program
+
+class HelloWorld{
+public static void main(String[] args)
+{
+    int a =5;
+    byte b = 2;
+    char c = 'f';
+    float d = 15;
+    long e = 78;
+    double f = 69.48;
+    String s ="Rajkumar";
+
+System.out.println("a : "+ a );
+System.out.println("b : "+ b);
+System.out.println("c : "+ c );
+System.out.println("d : "+ d );
+System.out.println("e : "+ e );
+System.out.println("f : "+ f );
+System.out.println("s : "+ s );
+}
+}
